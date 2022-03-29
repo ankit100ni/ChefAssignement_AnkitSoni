d2a3667435611128fb2581d6af8edb6052ea3909
Ankit <ankit10093@gmail.com>
2022-03-29 17:49:34 +0530

    update windows metadat file

--- 4_Windows/metadata.rb
+++ 4_Windows/metadata.rb
@@ -1,6 +1,6 @@
 name '4_Windows'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Ankit Soni'
+maintainer_email 'ankit100ni@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures 4_Windows'
 version '0.1.0'
