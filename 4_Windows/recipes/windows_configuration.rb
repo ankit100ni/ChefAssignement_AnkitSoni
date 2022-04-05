8016667a19a9532444cf8137b8c358d327760cc0
Ankit <ankit10093@gmail.com>
2022-04-05 22:17:14 +0530

    updated windows cookbook

---
+++ 4_Windows/recipes/windows_configuration.rb
@@ -0,0 +1,53 @@
+#
+# Cookbook:: 4_Windows
+# Recipe:: windows_configuration
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+
+windows_user_privilege node['Policy']['SeInteractiveLogonRight'] do
+  privilege 'SeInteractiveLogonRight'
+  users ['BUILTIN\Administrators']
+  action :set
+end
+
+windows_user_privilege node['Policy']['SeRemoteInteractiveLogonRight'] do
+  privilege 'SeRemoteInteractiveLogonRight'
+  users          ['BUILTIN\Administrators', 'BUILTIN\Remote Desktop Users']
+  action         :set
+end
+
+windows_security_policy node['Policy']['EnableAdminAccount'] do
+  secoption 'EnableAdminAccount'
+  secvalue '0'
+  action :set
+end
+
+registry_key node['Policy']['DisableCAD'] do
+  values [{
+      name: 'DisableCAD',
+      type: :dword,
+      data: 0,
+  }]
+  action :create
+end
+
+registry_key node['Registry']['NtpClinet'] do
+  values [{
+      name:  'Enabled',
+      type:  :dword,
+      data:  1,
+  }]
+  recursive true
+  action :create
+end
+
+registry_key node['Registry']['NtpServer'] do
+  values [{
+      name:  'Disabled',
+      type:  :dword,
+      data:  0,
+
+  }]
+  recursive true
+  action :create
+end
---
+++ 4_Windows/test/integration/default/windows_configuration_test.rb
@@ -0,0 +1,28 @@
+# Chef InSpec test for recipe 4_Windows::windows_configuration
+
+# The Chef InSpec reference, with examples and extensive documentation, can be
+# found at https://docs.chef.io/inspec/resources/
+
+describe security_policy do
+  its('SeInteractiveLogonRight') { should eq ['S-1-5-32-544'] }
+end
+
+describe security_policy do
+  its('SeRemoteInteractiveLogonRight') { should eq ['S-1-5-32-544'] + ['S-1-5-32-555'] }
+end
+
+describe security_policy do
+  its('EnableAdminAccount') { should eq 0 }
+end
+
+describe registry_key 'HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System' do
+  its('DisableCAD') { should eq 0 }
+end
+
+describe registry_key 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\W32time\\TimeProviders\\NtpClient' do
+  its('Enabled') { should eq 1 }
+end
+
+describe registry_key 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\W32time\\TimeProviders\\NTPServer' do
+  its('Disabled') { should eq 0 }
+end
