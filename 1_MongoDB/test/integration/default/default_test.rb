a6c1ba1e6a494e8c48a9b68904736c6e4f9e5dce
Ankit <ankit10093@gmail.com>
2022-04-05 21:06:28 +0530

    test for mongodb

--- 1_MongoDB/test/integration/default/default_test.rb
+++ 1_MongoDB/test/integration/default/default_test.rb
@@ -5,8 +5,3 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-describe service('mongod') do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
-end
